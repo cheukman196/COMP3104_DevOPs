0c484e9f51f4130bb0cacf97ba2ec34770e437fd
cheukman196 <cheukman196@live.com>
2024-10-02 20:11:15 -0400

    lab 5 update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101422756 || Cheuk Man Sit || DevOps</h5>
+      <h5>Developed By: 101422756 || Cheuk Man Sit || DevOps </h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
